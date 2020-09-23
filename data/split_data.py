e40ea38809c319a3019511965fad18b3f24ab8f2
jjerry-k <jaeyeol2931@gmail.com>
2020-09-23 23:57:39 +0900

    Update

--- data/split_data.py
+++ data/split_data.py
@@ -1,16 +1,29 @@
 import os
 import shutil
 import numpy as np
-src = 'data/Train'
-dst = 'data/Test'
-stones = sorted([stone for stone in os.listdir(src) if os.path.isdir(os.path.join(src, stone))]) 
+root = './Stone'
+train = './Train'
+test = './Test'
+stones = sorted([stone for stone in os.listdir(root) if os.path.isdir(os.path.join(root, stone))]) 
 
-ratio = 0.1
+ratio = 0.2
 
 for stone in stones:
-    os.makedirs(os.path.join(dst, stone), exist_ok=True)
-    stone_path = os.path.join(src, stone)
-    img_lists = sorted(os.listdir(stone_path))
-    indices = np.random.choice(len(img_lists), int(len(img_lists)*ratio), replace=False)
-    for idx in indices:
-        shutil.move(os.path.join(stone_path, img_lists[idx]), os.path.join(dst, stone, img_lists[idx]))
+    if stone == "chalcopyrite":
+        target = "chalcopyrite"
+    else:
+        target = "etc"
+    
+    stone_path = os.path.join(root, stone)
+    stone_img_lists = sorted(os.listdir(stone_path))
+    
+    train_stone_path = os.path.join(train, target)
+    test_stone_path = os.path.join(test, target)
+
+    threshold = int(len(stone_img_lists)*ratio)
+
+    for img in stone_img_lists[:threshold]:
+        shutil.move(os.path.join(stone_path, img), os.path.join(test_stone_path, img))
+
+    for img in stone_img_lists[threshold:]:
+        shutil.move(os.path.join(stone_path, img), os.path.join(train_stone_path, img))
